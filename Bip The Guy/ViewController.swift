fa6761a2d87e02f85fbeb5be26fe0fe9fef8a673
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-09-17 18:18:43 -0400

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Ben Meisenzahl on 9/17/18.
+//  Copyright © 2018 Ben Meisenzahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
